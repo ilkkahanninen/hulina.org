ac27333cee5b40a8c802330748421fc30e9db779
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2018-10-19 12:24:52 +0300

    Support both relative and absolute image paths

--- src/model/paths.js
+++ src/model/paths.js
@@ -1,7 +1,7 @@
 export const getImageFilename = (src, width) =>
   `${src.replace(/\.\w+$/, "")}${width ? `_${width}` : ""}.jpg`
 
-export const getImagePath = src => `/images/${src}`
+export const getImagePath = src => (src[0] === "/" ? src : `/images/${src}`)
 
 export const releasesPath = "/julkaisut"
 
